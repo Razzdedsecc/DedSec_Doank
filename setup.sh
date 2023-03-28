914708f2e46177d4ab07bfd64bbd8d7c2abe3c5a
Razzdedsecc <128728458+Razzdedsecc@users.noreply.github.com>
2023-03-28 22:25:16 +0800

    Raz??

---
+++ setup.sh
@@ -0,0 +1,32 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install git -y
+sudo apt-get install golang -y
+sudo apt-get install perl -y
+sudo apt-get install python3 -y
+sudo apt-get install python2 -y
+sudo apt-get install python3-pip -y
+sudo apt-get install nodejs -y
+sudo apt-get install npm -y
+npm i requests
+npm i https-proxy-agent
+npm i crypto-random-string
+npm i events
+npm i fs
+npm i net
+npm i cloudscraper
+npm i request
+npm i hcaptcha-solver
+npm i randomstring
+npm i cluster
+npm i cloudflare-bypasser
+npm i axios
+npm audit
+sudo pip install asciimatics
+sudo pip install pystyle
+sudo pip install integer
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get install ./google-chrome-stable_current_amd64.deb
+ulimit -n 999999
+chmod 777 *
+python3 main.py
